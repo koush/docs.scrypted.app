1dc97fa5e745535f90443c330ee9d120ef86b6cb
Koushik Dutta <koushd@gmail.com>
2024-03-01 19:45:08 -0800

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -20,12 +20,13 @@ export default withMermaid({
     },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Scrypted', link: '/' },
+      // { text: 'Scrypted', link: '/' },
       { text: 'Scrypted NVR', link: '/scrypted-nvr/' },
       { text: 'Installation', link: '/installation' },
       { text: 'Download', link: '/desktop-application' },
       { text: 'Camera Setup', link: '/camera-preparation' },
       { text: 'Buyer\'s Guide', link: '/buyers-guide/' },
+      { text: 'Scripts', link: 'https://scripts.scrypted.app' },
     ],
 
     sidebar: [
